273cda55acd110b7a026785b67b571b50a1129f4
Tao Zhou <57700360+infZhou@users.noreply.github.com>
2024-09-02 20:37:24 +0800

    Update pubs.js

--- pubs.js
+++ pubs.js
@@ -58,7 +58,7 @@ const PUBLICATIONS = [
     authors:
       "Tao Zhou, Qi Ye, Wenhan Luo, Haizhou Ran, Zhiguo Shi, Jiming Chen",
     image: "/resource/APPTrack+_Demo.gif",
-    conference: "submitted to IJCV 2024, under review",
+    conference: "IJCV 2024 (to appear)",
     links: {
       "project page": "",
       arxiv: "",
@@ -67,7 +67,7 @@ const PUBLICATIONS = [
       video: "",
       code: "",
     },
-    badges: [],
+    badges: ["CCF A"],
   },
   {
     title:
